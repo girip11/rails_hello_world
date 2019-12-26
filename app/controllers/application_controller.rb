52e27dd635d6ce640ede7e852995326c22b037f5
Girish Pasupathy <girip11@users.noreply.github.com>
2019-12-26 09:31:09 +0530

    Rails hello world application (#1)
    Changes to be committed:
    	new file:   .bundle/config
    	new file:   .gitignore
    	new file:   .irbrc
    	new file:   .prettierrc.json
    	new file:   .rubocop.yml
    	new file:   .ruby-version
    	new file:   .solargraph.yml
    	new file:   Gemfile
    	new file:   Gemfile.lock
    	modified:   README.md
    	new file:   Rakefile
    	new file:   app/assets/config/manifest.js
    	new file:   app/assets/images/.keep
    	new file:   app/assets/javascripts/application.js
    	new file:   app/assets/javascripts/cable.js
    	new file:   app/assets/javascripts/channels/.keep
    	new file:   app/assets/stylesheets/application.css
    	new file:   app/channels/application_cable/channel.rb
    	new file:   app/channels/application_cable/connection.rb
    	new file:   app/controllers/application_controller.rb
    	new file:   app/controllers/concerns/.keep
    	new file:   app/helpers/application_helper.rb
    	new file:   app/jobs/application_job.rb
    	new file:   app/mailers/application_mailer.rb
    	new file:   app/models/application_record.rb
    	new file:   app/models/concerns/.keep
    	new file:   app/views/layouts/application.html.erb
    	new file:   app/views/layouts/mailer.html.erb
    	new file:   app/views/layouts/mailer.text.erb
    	new file:   bin/bundle
    	new file:   bin/rails
    	new file:   bin/rake
    	new file:   bin/setup
    	new file:   bin/update
    	new file:   bin/yarn
    	new file:   config.ru
    	new file:   config/application.rb
    	new file:   config/boot.rb
    	new file:   config/cable.yml
    	new file:   config/database.yml
    	new file:   config/environment.rb
    	new file:   config/environments/development.rb
    	new file:   config/environments/production.rb
    	new file:   config/environments/test.rb
    	new file:   config/initializers/application_controller_renderer.rb
    	new file:   config/initializers/assets.rb
    	new file:   config/initializers/backtrace_silencers.rb
    	new file:   config/initializers/cookies_serializer.rb
    	new file:   config/initializers/filter_parameter_logging.rb
    	new file:   config/initializers/inflections.rb
    	new file:   config/initializers/mime_types.rb
    	new file:   config/initializers/wrap_parameters.rb
    	new file:   config/locales/en.yml
    	new file:   config/puma.rb
    	new file:   config/routes.rb
    	new file:   config/secrets.yml
    	new file:   config/spring.rb
    	new file:   db/seeds.rb
    	new file:   lib/assets/.keep
    	new file:   lib/tasks/.keep
    	new file:   log/.keep
    	new file:   package.json
    	new file:   public/404.html
    	new file:   public/422.html
    	new file:   public/500.html
    	new file:   public/apple-touch-icon-precomposed.png
    	new file:   public/apple-touch-icon.png
    	new file:   public/favicon.ico
    	new file:   public/robots.txt
    	new file:   rails_hello_world.code-workspace
    	new file:   test/application_system_test_case.rb
    	new file:   test/controllers/.keep
    	new file:   test/fixtures/.keep
    	new file:   test/fixtures/files/.keep
    	new file:   test/helpers/.keep
    	new file:   test/integration/.keep
    	new file:   test/mailers/.keep
    	new file:   test/models/.keep
    	new file:   test/system/.keep
    	new file:   test/test_helper.rb
    	new file:   tmp/.keep
    	new file:   vendor/.keep

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello_world
+    render html: "Hello World"
+  end
+end
